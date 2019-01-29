ef6b1ba197d4aa340e6651bb48c05b254f7a5e6e
Gerald collaku <gerald.collaku@exozet.com>
2019-01-29 15:11:44 +0100

    Initial Commit

---
+++ iOSCore/iOSCore.h
@@ -0,0 +1,19 @@
+//
+//  iOSCore.h
+//  iOSCore
+//
+//  Created by Gerald Çollaku on 29.01.19.
+//  Copyright © 2019 Exozet Berlin GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOSCore.
+FOUNDATION_EXPORT double iOSCoreVersionNumber;
+
+//! Project version string for iOSCore.
+FOUNDATION_EXPORT const unsigned char iOSCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSCore/PublicHeader.h>
+
+
